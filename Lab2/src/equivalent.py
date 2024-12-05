10de5d3f755f6b22ab9e80d2658d524a4ed61857
Kostya Kostikov <kostickov.costya@yandex.ru>
2024-12-06 00:11:29 +0300

    fixed equivalent.py

--- Lab2/src/equivalent.py
+++ Lab2/src/equivalent.py
@@ -38,14 +38,6 @@ def checkEquivalenceDFA(dfaMAT, dfaLearner):
     dfaLearner.Sigma = alphabet
     
     if dfaMAT == dfaLearner:
-def generateRandomWord(dfa):
-    ln = random.randint(3, 50)
-    words = dfa.enumDFA(idx)
-    idx = random.randint(0, len(words))
-    return words[idx]
-
-def checkEquivalence(dfaMAT, dfaLearner):
-    if dfaMAT == dfaLearner:
         return True, ''
 
     
@@ -60,11 +52,6 @@ def checkEquivalence(dfaMAT, dfaLearner):
     if dfaDif.countTransitions() == 0:
         # dfaMAT - подавтомат dfaLearner
         difDfa = dfaLearnerComplete & (~dfaMATComplete)
-    dfaDif = dfaMAT & (~dfaLearner)
-
-    if dfaDif.countTransitions() == 0:
-        # dfaMAT - подавтомат dfaLearner
-        difDfa = dfa2 & (~dfa1)
         return False, generateRandomWord(difDfa)
 
     return False, generateRandomWord(dfaDif)
